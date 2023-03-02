a1eea95d3d41ef019a2f539bceab908df4dab379
Akshay4955 <akshayshedge11@gmail.com>
2023-03-02 09:31:21 +0530

    Added: Code for temperature conversion

--- src/main/java/com/bridgelabz/junit_programs/Util.java
+++ src/main/java/com/bridgelabz/junit_programs/Util.java
@@ -4,10 +4,28 @@ import java.util.Scanner;
 
 public class Util {
     public static void main(String[] args) {
+
         getDay();
+        getConvertedTemperature();
     }
 
-    public static void getDay() {
+    static void getConvertedTemperature() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter option 1 for Celsius to Fahrenheit and 2 for Fahrenheit to Celsius ");
+        int option = input.nextInt();
+        System.out.println("Enter the temperature: ");
+        int temperature = input.nextInt();
+        switch (option) {
+            case 1 :
+                System.out.println("Celsius to Fahrenheit is equal to: " + ((temperature * 9 / 5) + 32));
+                break;
+            case 2:
+                System.out.println("Fahrenheit to Celsius is equal to: " + (temperature - 32) * 5 / 9);
+                break;
+        }
+    }
+
+    static void getDay() {
         Scanner input = new Scanner(System.in);
         System.out.println("Welcome to day of week");
         System.out.println("Enter the Day: ");
@@ -20,6 +38,7 @@ public class Util {
         int x = y0 + y0 / 4 - y0 / 100 + y0 / 400;
         int m0 = m + 12 * ((14 - m) / 12) - 2;
         int d0 = (d + x + 31 * m0 / 12) % 7;
+
         switch (d0) {
             case 0:
                 System.out.println("Sunday");
